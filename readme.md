a34a55363e104a2b52de171a7506699e94380aff
Maurice van Creij <maurice.vancreij@webqem.com>
2024-06-26 11:35:35 +1000

    update example

--- readme.md
+++ readme.md
@@ -2,3 +2,18 @@
 
 This is an early prototype for a cloud based multiplayer visual novel.
 
+## Project
+
+https://console.firebase.google.com/u/0/project/shared-xp/
+
+## Authentication
+
+https://firebase.google.com/docs/auth/web/password-auth#web
+
+## Read and Write
+
+https://firebase.google.com/docs/database/web/read-and-write
+
+## Rules
+
+https://firebase.google.com/docs/database/security/rules-conditions
--- scripts.js
+++ scripts.js
@@ -1,40 +1,68 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-lite.js";
-import { getDatabase, ref, child, set, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase, ref, child, set, get, push, query, onValue, orderByChild } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 export class SharedXP {
 	constructor(props) {
 		this.app = initializeApp({
 			apiKey: "AIzaSyDBi_MCSCj7rolI84P6kdie30gNnHrRJiY",
 			authDomain: "shared-xp.firebaseapp.com",
+			databaseURL: "https://shared-xp-default-rtdb.asia-southeast1.firebasedatabase.app",
 			projectId: "shared-xp",
 			storageBucket: "shared-xp.appspot.com",
 			messagingSenderId: "692502370561",
-			appId: "1:692502370561:web:969068a94f2193c4782300"
+			appId: "1:692502370561:web:969068a94f2193c4782300",
 		});
 
-		//this.db = getFirestore(this.app);
-
+		this.auth = getAuth();
 		this.db = getDatabase(this.app);
-
 		console.log("sharedXP:", this.app, this.db);
 
-		this.writeData('text_1', "Lorem Ipsum", "Lorem ipsum dolor sit amet 2.");
+		this.existingAccount('woolly.mittens@gmail.com', 'H4r4b1b1@@', (credential) => {
+			this.writeData("channel", "Lorem Ipsum", "Lorem ipsum dolor sit amet 5.");
+			this.readData("channel");
+		});
+	}
 
-		this.readData('text_1');
+	async newAccount(email, password, handler) {
+		const credential = await createUserWithEmailAndPassword(this.auth, email, password);
+		console.log("create credential", credential);
+		handler(credential);
+	}
+
+	async existingAccount(email, password, handler) {
+		const credential = await signInWithEmailAndPassword(this.auth, email, password);
+		console.log("login credential", credential);
+		handler(credential);
 	}
 
 	async writeData(lobby, username, message) {
-		const result = await set(ref(this.db, "posts/" + lobby), {
+		/*
+		await set(push(ref(this.db, `posts/${lobby}`)), {
 			username: username,
-			message: message
+			message: message,
+			time: new Date().getTime(),
 		});
-		console.log('result', result);
+		*/
+		await set(ref(this.db, `posts/${lobby}`), {
+			username: username,
+			message: message,
+			time: new Date().getTime(),
+		});
 	}
 
 	async readData(lobby) {
-		const result = await get(child(ref(this.db), `posts/${lobby}`));
-		console.log('result', result.val());
+		onValue(
+			query(ref(this.db, `posts/${lobby}`, orderByChild("time"))),
+			(snapshot) => {
+				snapshot.forEach((snapshot) => {
+					console.log("snapshot", snapshot.key, snapshot.val());
+				});
+			},
+			{
+				onlyOnce: true,
+			}
+		);
 	}
 }
 
